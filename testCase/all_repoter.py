825f4297cedd8c794dcb2ab2d2c27b01499f002e
jinjiawu <734561145@qq.com>
2017-09-30 15:52:10 +0800

    代码更新上传

--- testCase/all_repoter.py
+++ testCase/all_repoter.py
@@ -12,8 +12,8 @@ class Get_All_repoter(unittest.TestCase):
     '''
     获取所有的报告
     '''
-
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         #所有接口名字存放
         self.names = []
         #所有接口地址存放
@@ -24,6 +24,8 @@ class Get_All_repoter(unittest.TestCase):
         self.all_codes = []
         #所有测试结果存放pass或者fail
         self.all_result = []
+        #测试报表的title
+        self.titles = ('接口名','接口地址','接口数据','返回值','测试结果')
         #把表中所有的数据都读出来
         self.file_contains = Read_xls_file('all.xlsx').get_xls()
         for i in self.file_contains:
@@ -32,25 +34,54 @@ class Get_All_repoter(unittest.TestCase):
             self.datas.append(i[2])
             logger.info('%s信息获取完毕'%i[0])
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         pass
 
-    def est_get_all(self):
+    def test_get_all(self):
         for n in range(len(self.urls)):
             results = requests.get(self.urls[n],params=eval(self.datas[n]))
-            self.assertEqual(results.status_code,200,msg='code error')
-            self.all_codes.append(results.status_code)#将所有返回的状态码存进all_codes里面
-            if results.status_code == 200:
+            try:
+                self.assertEqual(results.json()['resultcode'],'200',msg='code error')
+            except AssertionError as A:
+                logger.info(A)
+            self.all_codes.append(results.json()['resultcode'])#将所有返回的状态码存进all_codes里面
+            if results.json()['resultcode'] == '200':
                 self.all_result.append('pass')
             else:
                 self.all_result.append('fail')
-            print(results.json())
+        #将所有数据存放在all_files中
+        all_files = list(zip(self.names,self.urls,self.datas,self.all_codes,self.all_result))
+        all_files.insert(0,self.titles)
+        return all_files
 
     def test_write_to_excel(self):
-        all_files = list(zip(self.names,self.urls,self.datas))
+        all_files = self.test_get_all()
         file_path = os.path.dirname(os.path.abspath('.'))+'/repoter/repoter.xls'
         wb = xlwt.Workbook()
         sheet = wb.add_sheet('All_repoter')#sheet的名称为All_repoter
+
+        #单元格的格式
+        pass_style = 'pattern:pattern solid,fore_colour green;font: bold on;'#背景颜色绿色，粗体字
+        pass_header_style = xlwt.easyxf(pass_style)
+
+        fail_style = 'pattern:pattern solid,fore_colour red;font: bold on;'#背景颜色红色，粗体字
+        fail_header_style = xlwt.easyxf(fail_style)
+
+        #获取行数
+        row_count = len(all_files)
+        #获取列数
+        col_count = len(all_files[0])
+
+        for row in range(row_count):
+            for col in range(col_count):
+                if all_files[row][col] == 'pass':#如果测试通过就标绿色
+                    sheet.write(row, col, all_files[row][col], pass_header_style)
+                elif all_files[row][col] == 'fail':#如果测试失败就标红色
+                    sheet.write(row, col, all_files[row][col],fail_header_style)
+                else:
+                    sheet.write(row, col, all_files[row][col])
+        wb.save(file_path)
 
 
 if __name__ == '__main__':
